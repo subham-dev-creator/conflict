b6c7debae06b3e186bec7363e71870e6d2aec139
subhamstark <subhamofficial98@gmail.com>
2021-02-07 23:38:45 +0530

    if else

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is Shubham");
-        System.out.println("I am in a Tech Talk");
+        if(2+2==4)
+            System.out.println("Correct");
+        else
+            System.out.println("Wrong repeat your schooling ;) ");
     }
 }
